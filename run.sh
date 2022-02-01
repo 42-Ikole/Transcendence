ed2065192b8006e2ba06aff773e8ed7d7a1ebfdc
Ingmar Kole <ikole@student.codam.nl>
2022-02-01 18:05:15 +0100

    Feat: created a 'run' script

---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  #
+#                                                                              #
+#               __    __   ______    ______   ________  _______                #
+#              /  \  /  | /      \  /      \ /        |/       \               #
+#              $$  \ $$ |/$$$$$$  |/$$$$$$  |$$$$$$$$/ $$$$$$$  |              #
+#              $$$  \$$ |$$ |  $$/ $$ |  $$ |   $$ |   $$ |__$$ |              #
+#              $$$$  $$ |$$ |      $$ |  $$ |   $$ |   $$    $$<               #
+#              $$ $$ $$ |$$ |   __ $$ |  $$ |   $$ |   $$$$$$$  |              #
+#              $$ |$$$$ |$$ \__/  |$$ \__$$ |   $$ |   $$ |__$$ |              #
+#              $$ | $$$ |$$    $$/ $$    $$/    $$ |   $$    $$/               #
+#              $$/   $$/  $$$$$$/   $$$$$$/     $$/    $$$$$$$/                #
+#                                                                              #
+#                                                                              #
+#                           New Coders on the Block                            #
+#    ——————————————————————————————————————————————————————————————————————    #
+#                 Mpeerdem   |   https://github.com/Maqrkk                     #
+#                 Rpet       |   https://github.com/rpet91                     #
+#                 Mraasvel   |   https://github.com/mraasvel                   #
+#                 Ikole      |   https://github.com/K1ngmar                    #
+#                 Nvan-aac   |   https://github.com/vanaacken                  #
+#                                                                              #
+# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  #
+
+# create symlink to goinfre
+bash src/scripts/create_symlink.sh
+
+# run docker compose
+docker-compose up --build
