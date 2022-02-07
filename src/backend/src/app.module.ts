2b1c535da26b24fc4b7f347a567ebdd2b62d75ca
Niels Van aacken <nvan-aac@f0r6s1.codam.nl>
2022-02-07 16:29:13 +0100

    update

--- src/backend/src/app.module.ts
+++ src/backend/src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Connection } from 'typeorm'
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'pong_db',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [User],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection){}
+}
