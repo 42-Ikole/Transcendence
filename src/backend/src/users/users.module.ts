b6a9b0334587813fa357b6194037a0ee814d7028
vanaacken <nielsvanaacken@hotmail.com>
2022-02-10 11:24:42 +0100

    update

--- src/backend/src/users/users.module.ts
+++ src/backend/src/users/users.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { UsersService } from './users/users.service';
 
 @Module({
   controllers: [UsersController],
