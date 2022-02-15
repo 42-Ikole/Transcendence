ae0b3d2265165089e145ca06244388b1dde2f9a6
Ingmar Kole <ikole@student.codam.nl>
2022-02-15 12:33:07 +0100

    Chore: indentation

--- src/backend/app/src/users/entities/user.entity.ts
+++ src/backend/app/src/users/entities/user.entity.ts
@@ -2,15 +2,18 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn()
-  id: number;
+	@PrimaryGeneratedColumn()
+	id: number;
 
-  @Column()
-  firstName: string;
+	@Column()
+	username: string;
 
-  @Column()
-  lastName: string;
+	@Column()
+	firstName: string;
 
-  @Column({ default: true })
-  isActive: boolean;
+	@Column()
+	lastName: string;
+
+	@Column({ default: true })
+	isActive: boolean;
 }
