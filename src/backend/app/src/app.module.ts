310029d283fc396f829a4fb0713007c4ea4e1032
Ingmar Kole <ikole@student.codam.nl>
2022-02-17 12:54:12 +0100

    Chore: linter

--- src/backend/app/src/app.module.ts
+++ src/backend/app/src/app.module.ts
@@ -1,9 +1,8 @@
-
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { config } from "./orm/ormconfig";
+import { config } from './orm/ormconfig';
 
 @Module({
   imports: [TypeOrmModule.forRoot(config)],
