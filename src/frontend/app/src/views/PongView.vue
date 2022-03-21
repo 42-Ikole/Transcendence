1bafab83e5247127a111dc2fddce686952ee8090
mraasvel <mraasvel@student.codam.nl>
2022-03-21 22:16:28 +0100

    feat: pong frontend rendering

---
+++ src/frontend/app/src/views/PongView.vue
@@ -0,0 +1,14 @@
+<template>
+	<PongGame />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import PongGame from "@/components/Pong/PongGame.vue"
+
+export default defineComponent({
+	components: {
+		PongGame
+	},
+});
+</script>
