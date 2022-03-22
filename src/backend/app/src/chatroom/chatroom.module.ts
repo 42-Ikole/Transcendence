57e02539572db542b48dfa38424e9efa0750ef27
Mark Peerdeman <markpeerdeman@live.nl>
2022-03-22 07:41:29 +0100

    chore: removed commented placeholder files

--- src/backend/app/src/chatroom/chatroom.module.ts
+++
@@ -1,7 +0,0 @@
-// import { Module } from '@nestjs/common';
-// import { ChatroomController } from './chatroom.controller';
-
-// @Module({
-//   controllers: [ChatroomController]
-// })
-// export class ChatroomModule {}
