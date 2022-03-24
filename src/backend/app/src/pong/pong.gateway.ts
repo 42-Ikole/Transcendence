cf6935a207174f720e200dfff8d17988a30bfc1e
mraasvel <mbraasveldt@gmail.com>
2022-03-24 12:59:59 +0100

    feat: pong service for abstraction of data structures

--- src/backend/app/src/pong/pong.gateway.ts
+++ src/backend/app/src/pong/pong.gateway.ts
@@ -1,11 +1,4 @@
 import {
-  UseGuards,
-  UsePipes,
-  ValidationPipe,
-  UseFilters,
-} from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import {
   WebSocketServer,
   WebSocketGateway,
   OnGatewayConnection,
@@ -15,15 +8,9 @@ import {
   MessageBody,
 } from '@nestjs/websockets';
 import { Server } from 'socket.io';
-import { SessionUser } from 'src/auth/auth.types';
-import { UserService } from 'src/user/user.service';
-import { decodeCookie } from '../websocket/cookie';
-import { WebsocketGuard } from '../websocket/websocket.guard';
 import { RequestMatchDto, SocketWithUser } from '../websocket/websocket.types';
-import { WsExceptionFilter } from '../websocket/websocket.exception.filter';
 import { GameRoom, GameState } from './pong.types';
 import { gameHasEnded, movePlayer, newGameState, updateGamestate } from './pong.game';
-import { ClientRequest } from 'http';
 import { PongService } from './pong.service';
 
 /*
@@ -41,120 +28,48 @@ Endpoints:
 })
 export class PongGateway implements OnGatewayConnection, OnGatewayDisconnect {
   constructor(
-    private userService: UserService,
-    private configService: ConfigService,
     private pongService: PongService,
   ) {}
 
   @WebSocketServer() wss: Server;
-  waitingUser: SocketWithUser | null;
-  gameRooms: Record<string, GameRoom> = {}; // roomName -> extra room Data
-  sockets: Record<string, SocketWithUser> = {}; // socketId -> User Socket, TODO: should be userId and multiple logins by same user should be denied
-  socketIds: Record<number, string> = {}; // userId -> socketId
-  disconnectedUsers: Record<number, string> = {}; // userId -> roomName
-
-  private async userFromCookie(cookie: string) {
-    const sessionUser: SessionUser = await decodeCookie(
-      cookie,
-      this.configService,
-    );
-    if (!sessionUser) {
-      return null;
-    }
-    const user = await this.userService.findById(sessionUser.id);
-    return user;
-  }
 
   async handleConnection(client: SocketWithUser) {
-    client.user = await this.userFromCookie(client.handshake.headers.cookie);
+    client.user = await this.pongService.userFromCookie(client.handshake.headers.cookie);
     if (!client.user) {
       console.log("/pong connection denied: NO USER FOUND:", client.id);
       client.disconnect();
-    } else {
-      console.log('/pong connect:', client.user.username);
-      this.sockets[client.id] = client;
+      return;
+    }
+    console.log('/pong connect:', client.user.username);
+    this.pongService.addClient(client);
+    if (this.pongService.isDisconnected(client)) {
       this.handleReconnect(client);
     }
   }
 
   handleReconnect(client: SocketWithUser) {
-    const roomName = this.disconnectedUsers[client.user.id];
-    if (roomName && this.gameRooms[roomName]) {
-      console.log("reconnecting", client.user.username, "to", roomName);
-      if (this.gameRooms[roomName].playerOne.disconnected) {
-        this.gameRooms[roomName].playerOne.socketId = client.id;
-        this.gameRooms[roomName].playerOne.disconnected = false;
-      } else {
-        this.gameRooms[roomName].playerTwo.socketId = client.id;
-        this.gameRooms[roomName].playerTwo.disconnected = false;
-      }
-      client.gameRoom = roomName;
-      client.join(roomName);
-      client.emit('startGame');
-    }
-    if (roomName) {
-      this.deleteDisconnectedUser(client.user.id);
-    }
+    this.pongService.reconnectUser(client);
+    client.join(client.gameRoom);
+    client.emit('startGame');
   }
 
   handleDisconnect(client: SocketWithUser) {
-    if (this.waitingUser && client.id === this.waitingUser.id) {
-      console.log(client.user.username, "stopped searching");
-      this.waitingUser = null;
-    } else if (client.gameRoom) {
-      console.log(client.user.username, "disconnected from game:", client.gameRoom);
-      this.handleGameDisconnect(client);
+    if (this.pongService.isPlaying(client)) {
+      this.pongService.disconnectUser(client);
+      if (this.pongService.bothPlayersDisconnected(client.gameRoom)) {
+        console.log("both players disconnected, removing gameRoom:", client.gameRoom);
+        this.deleteGame(client.gameRoom);
+      }
     }
-    delete this.sockets[client.id];
+    this.pongService.removeClient(client);
     console.log('/pong Disconnect:', client.user.username);
   }
 
-  handleGameDisconnect(client: SocketWithUser) {
-    if (!this.gameRooms[client.gameRoom]) {
-      return;
-    }
-    this.disconnectedUsers[client.user.id] = client.gameRoom;
-    const gameRoom = this.gameRooms[client.gameRoom];
-    this.setDisconnectedFlag(client.gameRoom, client.id);
-    if (gameRoom.playerOne.disconnected && gameRoom.playerTwo.disconnected) {
-      console.log("both players disconnected, removing gameRoom:", client.gameRoom);
-      this.deleteGame(client.gameRoom);
-    }
-  }
-
-  setDisconnectedFlag(roomName: string, socketId: string) {
-    const gameRoom = this.gameRooms[roomName];
-    if (gameRoom.playerOne.socketId === socketId) {
-      gameRoom.playerOne.disconnected = true;
-    } else if (gameRoom.playerTwo.socketId === socketId) {
-      gameRoom.playerTwo.disconnected = true;
-    } else {
-      console.error("cannot find disconnected game for:", socketId, "in room", roomName);
-    }
-  }
-
-  deleteDisconnectedUser(id: number) {
-    if (this.disconnectedUsers[id]) {
-      console.log("deleting disconnected user:", id);
-      delete this.disconnectedUsers[id];
-    }
-  }
-
   deleteGame(roomName: string) {
-    this.deleteDisconnectedUser(this.gameRooms[roomName].playerOne.userId);
-    this.deleteDisconnectedUser(this.gameRooms[roomName].playerTwo.userId);
-    this.wss.to(roomName).emit("endGame", this.gameRooms[roomName].gameState);
-    clearInterval(this.gameRooms[roomName].intervalId);
-    this.deleteRoom(roomName);
-  }
-
-  // For each client in said room
-  deleteRoom(roomName: string) {
+    const gameState = this.pongService.getGameState(roomName);
+    this.wss.to(roomName).emit("endGame", gameState);
+    this.pongService.deleteGameRoom(roomName);
     this.wss.socketsLeave(roomName);
-    const gameRoom = this.gameRooms[roomName];
-    this.sockets[gameRoom.playerOne.socketId].gameRoom = null;
-    this.sockets[gameRoom.playerTwo.socketId].gameRoom = null;
-    delete this.gameRooms[roomName];
   }
 
   endGame(roomName: string) {
@@ -181,20 +96,19 @@ export class PongGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   // Check if there is another client ready to play, otherwise set client as waiting/searching
   matchMaking(client: SocketWithUser) {
-	  if (!this.waitingUser) {
+	  if (!this.pongService.canMatch(client)) {
       console.log(client.user.username, "is searching");
-		  this.waitingUser = client;
-	  } else if (this.waitingUser.id != client.id) {
-      // TODO: disallow a user to play against themselves, for now it is useful for testing
-      // User shouldn't be able to search while they are already searching on a different machine
-      console.log("found match");
-      this.startNewGame(this.waitingUser, client);
-      this.waitingUser = null;
+      this.pongService.enterMatchmakingQueue(client);
+      return;
+	  }
+    const matchedUser = this.pongService.getMatch(client);
+    if (matchedUser.id === client.id) {
+      console.error("client matched with itself");
+      return;
     }
-  }
-
-  generateRoomName() {
-    return '_' + Math.random().toString(36).slice(2, 9);
+    // TODO: disallow a user to play against themselves, for now it is useful for testing
+    // User shouldn't be able to search while they are already searching on a different machine
+    this.startNewGame(matchedUser, client);
   }
 
   // Create a new unique room for these clients to play in
@@ -205,21 +119,20 @@ export class PongGateway implements OnGatewayConnection, OnGatewayDisconnect {
       clientOne.emit("exception", "WARNING: you are playing with yourself");
       clientTwo.emit("exception", "WARNING: you are playing with yourself");
     }
-    const roomName = this.generateRoomName();
+    const roomName = this.pongService.generateRoomName();
     console.log(clientOne.user.username, "vs", clientTwo.user.username, "in", roomName);
     this.joinRoom(clientOne, roomName);
     this.joinRoom(clientTwo, roomName);
     this.wss.to(roomName).emit('startGame');
     const gameState = newGameState(clientOne.user.username, clientTwo.user.username);
     const intervalId = this.startGameLoop(roomName, gameState);
-    // Room data
-    this.gameRooms[roomName] = {
+    this.pongService.addGameRoom(roomName, {
       intervalId,
       playerOne: { socketId: clientOne.id, userId: clientOne.user.id, disconnected: false },
       playerTwo: { socketId: clientTwo.id, userId: clientTwo.user.id, disconnected: false },
       observers: [],
       gameState,
-    };
+    });
   }
 
   joinRoom(client: SocketWithUser, roomName: string) {
@@ -245,7 +158,7 @@ export class PongGateway implements OnGatewayConnection, OnGatewayDisconnect {
     if (!room) {
       return;
     }
-    const gameRoom = this.gameRooms[room];
+    const gameRoom = this.pongService.getGameRoom(room);
     if (client.id === gameRoom.playerOne.socketId) {
       movePlayer(gameRoom.gameState.playerOne.bar, data);
     } else if (client.id === gameRoom.playerTwo.socketId) {
--- src/backend/app/src/pong/pong.service.ts
+++ src/backend/app/src/pong/pong.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { SocketWithUser } from 'src/websocket/websocket.types';
 import { GameRoom } from './pong.types';
+import { Server } from 'socket.io';
+import { SessionUser } from 'src/auth/auth.types';
+import { ConfigService } from '@nestjs/config';
+import { UserService } from 'src/user/user.service';
+import { decodeCookie } from 'src/websocket/cookie';
 
 @Injectable()
 export class PongService {
-  constructor() {}
+  constructor(
+    private configService: ConfigService,
+    private userService: UserService,
+  ) {}
 
-  private waitingUser: SocketWithUser | null;
+  private waitingUser: SocketWithUser | null = null;
   private gameRooms: Record<string, GameRoom> = {}; // roomName -> extra room Data
   private sockets: Record<string, SocketWithUser> = {}; // socketId -> User Socket, TODO: should be userId and multiple logins by same user should be denied
   private socketIds: Record<number, string> = {}; // userId -> socketId
@@ -16,8 +24,50 @@ export class PongService {
     this.sockets[client.id] = client;
   }
 
+  async userFromCookie(cookie: string) {
+    const sessionUser: SessionUser = await decodeCookie(
+      cookie,
+      this.configService,
+    );
+    if (!sessionUser) {
+      return null;
+    }
+    const user = await this.userService.findById(sessionUser.id);
+    return user;
+  }
+
   removeClient(client: SocketWithUser) {
+    if (this.waitingUser && this.waitingUser.id === client.id) {
+      console.log(client.user.username, "stopped searching");
+      this.waitingUser = null;
+    }
     delete this.socketIds[client.id];
+  }
+
+  isPlaying(client: SocketWithUser) {
+    return !!client.gameRoom;
+  }
+
+  disconnectUser(client: SocketWithUser) {
+    console.log(client.user.username, "disconnected from game:", client.gameRoom);
+    this.disconnectedUsers[client.user.id] = client.gameRoom;
+    this.setDisconnectedFlag(client.gameRoom, client.id);
+  }
+
+  bothPlayersDisconnected(roomName: string) {
+    const gameRoom = this.gameRooms[roomName];
+    return gameRoom.playerOne.disconnected && gameRoom.playerTwo.disconnected;
+  }
+
+  setDisconnectedFlag(roomName: string, socketId: string) {
+    const gameRoom = this.gameRooms[roomName];
+    if (gameRoom.playerOne.socketId === socketId) {
+      gameRoom.playerOne.disconnected = true;
+    } else if (gameRoom.playerTwo.socketId === socketId) {
+      gameRoom.playerTwo.disconnected = true;
+    } else {
+      console.error("cannot find disconnected game for:", socketId, "in room", roomName);
+    }
   }
 
   enterMatchmakingQueue(client: SocketWithUser) {
@@ -34,16 +84,51 @@ export class PongService {
     return waiting;
   }
 
-  disconnectUser(client: SocketWithUser) {
-    if (!client.gameRoom) {
-      return;
-    }
-    this.disconnectedUsers[client.user.id] = client.gameRoom;
+  isDisconnected(client: SocketWithUser): boolean {
+    return !!this.disconnectedUsers[client.user.id];
   }
 
+  deleteDisconnectedUser(userId: number) {
+    delete this.disconnectedUsers[userId];
+  }
+
+  deleteGameRoom(roomName: string) {
+    clearInterval(this.gameRooms[roomName].intervalId);
+    this.deleteDisconnectedUser(this.gameRooms[roomName].playerOne.userId);
+    this.deleteDisconnectedUser(this.gameRooms[roomName].playerTwo.userId);
+    const gameRoom = this.gameRooms[roomName];
+    this.sockets[gameRoom.playerOne.socketId].gameRoom = null;
+    this.sockets[gameRoom.playerTwo.socketId].gameRoom = null;
+    delete this.gameRooms[roomName];
+  }
+
+  // Should only call IF isDisconnected returns TRUE
   reconnectUser(client: SocketWithUser) {
     const roomName = this.disconnectedUsers[client.user.id];
     client.gameRoom = roomName;
-    delete this.disconnectedUsers[client.user.id];
+    if (this.gameRooms[roomName].playerOne.disconnected) {
+      this.gameRooms[roomName].playerOne.socketId = client.id;
+      this.gameRooms[roomName].playerOne.disconnected = false;
+    } else {
+      this.gameRooms[roomName].playerTwo.socketId = client.id;
+      this.gameRooms[roomName].playerTwo.disconnected = false;
+    }
+    this.deleteDisconnectedUser(client.user.id);
+  }
+
+  generateRoomName() {
+    return '_' + Math.random().toString(36).slice(2, 9);
+  }
+
+  addGameRoom(roomName: string, gameRoom: GameRoom) {
+    this.gameRooms[roomName] = gameRoom;
+  }
+
+  getGameRoom(roomName: string) {
+    return this.gameRooms[roomName];
+  }
+
+  getGameState(roomName: string) {
+    return this.gameRooms[roomName].gameState;
   }
 }
