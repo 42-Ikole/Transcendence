df7d2714ce61ce50f6fe6214b4f40437eb19ad3a
Ingmar Kole <ikole@student.codam.nl>
2022-03-30 14:50:30 +0200

    Feat: MiniProfile

---
+++ src/frontend/app/src/components/profile/MiniProfile.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="MiniProfile">
+	</div>
+</template>
+
+<style>
+.MiniProfile {
+	background: #f0f0f0 url("@/assets/profileplaceholder.jpeg");
+	background-repeat: no-repeat;
+	background-size: cover;
+	border-radius: 50%;
+	height: 75px;
+	width: 75px;
+}
+</style>
