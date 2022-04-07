7f6f50cc2571bd7cf6dd2c329f4fc3674f322cad
Remco Pet <rpet@student.codam.nl>
2022-04-07 13:11:26 +0200

    Feat: Better CSS for creating a chat room

--- src/frontend/app/src/components/chatrooms/CreateRoom.vue
+++ src/frontend/app/src/components/chatrooms/CreateRoom.vue
@@ -1,33 +1,35 @@
 <template>
-	<div v-if="isNotCreated">
+	<div class="card-header align-items-center" v-if="isNotCreated">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
-		<form class="conv_creation" @submit.prevent="createChat">
-			<h3>Creating a new chat room.</h3>
-			<div id="chosing_type">
-				<div class="one_elem">
-					<input type="radio" value="public" v-model="type" />
-					<label for="public">Public</label>
+		<form class="card-header justify-content-between align-items-center p-3" style="width: 40%; background-color: #eee;" @submit.prevent="createChat">
+			<h3 >Creating a new chat room.</h3>
+			<div class="card card-body">
+				<div class="form-check">
+					<input class="form-check-input" type="radio" value="public" v-model="type" />
+					<label class="form-check-label" for="public">Public</label>
 				</div>
-				<div class="one_elem">
-					<input type="radio" value="private" v-model="type" />
-					<label for="private">Private</label>
+				<div class="form-check">
+					<input class="form-check-input" type="radio" value="private" v-model="type" />
+					<label class="form-check-label" for="private">Private</label>
 				</div>
-				<div class="one_elem">
-					<input type="radio" value="protected" v-model="type" />
-					<label for="protected">Protected</label>
+				<div class="form-check">
+					<input class="form-check-input" type="radio" value="protected" v-model="type" />
+					<label class="form-check-label" for="protected">Protected</label>
 				</div>
 			</div>
-			<div>
-				Room name: <input class="input_name" placeholder="Type here" type="text" v-model="name" />
+			<div class="card-header justify-content-between">
+				Room name: <input class="card-title col-7" placeholder="Type here" type="text" v-model="name" />
+				<div v-if="type === 'protected'">
+					Room password:
+					<input class="card-title"  style="margin: 0px 7.5px 0px 7.5px;" placeholder="Password" :type="passwordVisibility" v-model="pass" />
+					<button type="button" @click="toggleShowPassword" >
+						<i class="fas" :class="{ 'fa-eye-slash': showPassword, 'fa-eye': !showPassword }"></i>
+					</button>
+				</div>
+				<div>
+					<input class="btn btn-success" style="line-height: 1;" v-bind:disabled="invalidRoomInput === true" type="submit" value="Create" />
+				</div>
 			</div>
-			<div v-if="type === 'protected'">
-				Room password:
-				<input class="input_pass" placeholder="Password" :type="passwordVisibility" v-model="pass" />
-				<button class="button" type="button" @click="toggleShowPassword" >
-					<i class="fas" :class="{ 'fa-eye-slash': showPassword, 'fa-eye': !showPassword }"></i>
-				</button>
-			</div>
-			<input v-bind:disabled="invalidRoomInput === true" type="submit" value="Create" />
 		</form>
 	</div>
 	<div v-else-if="isCreated">
@@ -109,34 +111,3 @@ export default defineComponent({
 })
 
 </script>
-
-<style scoped>
-
-	.conv_creation {
-		width: 380px;
-		height: 200px;
-		padding: 0px 10px;
-		margin: auto;
-		border: 2.5px solid #000044;
-		text-align: left;
-	}
-
-	#chosing_type {
-		display: flex;
-	}
-
-	.one_elem {
-		flex-direction: column;
-		margin: 5px 0px;
-		margin-right: 15px;
-	}
-
-	.input_name {
-		margin: 5px 0px;
-	}
-
-	.input_pass {
-		margin: 5px 0px;
-	}
-	
-</style>
