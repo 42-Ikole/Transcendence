a0111d615b4bc201f9d6cd90d0881272bd4ac972
Ingmar Kole <ikole@student.codam.nl>
2022-04-08 12:25:50 +0200

    feat: setup for file upload

--- src/frontend/app/src/views/ProfileView.vue
+++ src/frontend/app/src/views/ProfileView.vue
@@ -3,7 +3,12 @@
     <div class="row">
       <div class="col-md-5">
         <div class="profileImg" v-bind:class="statusStyling"></div>
-      </div>
+		<!-- blub !-->
+		<form>
+			<input type="file" class="form-control" id="customFile" v-on:change="updateAvatar($event)" />
+		</form>
+
+	  </div>
       <div class="col-md-7">
         <h1>{{ userName }}</h1>
         <p class="status" v-bind:class="statusStyling">{{ userStatus }}</p>
@@ -13,6 +18,48 @@
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { mapState } from "pinia";
+import { useUserStore } from "@/stores/UserStore";
+import MatchHistory from "@/components/profile/MatchHistory.vue";
+
+export default defineComponent({
+	data() {
+    return {
+      avatar: null,
+    };
+  },
+  computed: {
+    ...mapState(useUserStore, ["profileData", "state"]),
+    userName() {
+      return this.profileData.username;
+    },
+    userStatus() {
+      return this.state;
+    },
+    statusStyling() {
+      if (this.state === "ONLINE") return "status-online";
+      else if (this.state == "PLAYING") return "status-ingame";
+      else return "status-offline";
+    },
+  },
+  methods: {
+	  async updateAvatar(event) {
+		  const file = event.target.files[0];
+		  const reader = new FileReader();
+		  reader.readAsBinaryString(file);
+		  reader.onloadend = function(){
+			  // upload file -> reader.result
+		  }
+	  }
+  },
+  components: {
+    MatchHistory,
+  },
+});
+</script>
 
 <style>
 .profileImg {
@@ -44,30 +91,3 @@
   font-size: 12px;
 }
 </style>
-
-<script lang="ts">
-import { defineComponent } from "vue";
-import { mapState } from "pinia";
-import { useUserStore } from "@/stores/UserStore";
-import MatchHistory from "@/components/profile/MatchHistory.vue";
-
-export default defineComponent({
-  computed: {
-    ...mapState(useUserStore, ["profileData", "state"]),
-    userName() {
-      return this.profileData.username;
-    },
-    userStatus() {
-      return this.state;
-    },
-    statusStyling() {
-      if (this.state === "ONLINE") return "status-online";
-      else if (this.state == "PLAYING") return "status-ingame";
-      else return "status-offline";
-    },
-  },
-  components: {
-    MatchHistory,
-  },
-});
-</script>
