48619ea657fe74d702900a34fc117375f26b34fb
Mark Peerdeman <markpeerdeman@live.nl>
2022-04-08 09:25:34 +0200

    Feat: added /chat/users/{name}, and changes /chat/{name} to /chat/messages/{name}

--- src/backend/app/src/chat/chat.controller.ts
+++ src/backend/app/src/chat/chat.controller.ts
@@ -3,6 +3,8 @@ import { Controller, Get, Post, Body, Param, NotFoundException } from "@nestjs/c
 import { ChatService } from "./chat.service";
 import { CreateChatDto } from "./chat.types";
 import { Chat } from "src/orm/entities/chat.entity";
+import { User } from "src/orm/entities/user.entity";
+import { Message } from "src/orm/entities/message.entity";
 
 @ApiTags('chat')
 @Controller('chat')
@@ -14,18 +16,32 @@ export class ChatController {
 		return await this.chatService.findAll();
 	}
 
-	@Get(':name')
+	@Get('/messages/:name')
 	@ApiParam({
 		name: 'name',
 		required: true,
 		description: 'Name of a chatroom',
 		type: String,
 	})
-	async findByName(@Param('name') name: string): Promise<Chat> {
+	async findByName(@Param('name') name: string): Promise<Message[]> {
 		const chat: Chat = await this.chatService.findByName(name, ['messages', 'messages.author']);
 		if (chat === undefined)
 			throw new NotFoundException();
-		return chat;
+		return chat.messages;
+	}
+
+	@Get('/users/:name')
+	@ApiParam({
+		name: 'name',
+		required: true,
+		description: 'Name of a chatroom',
+		type: String,
+	})
+	async findUsersForChat(@Param('name') name: string): Promise<User[]> {
+		const chat: Chat = await this.chatService.findByName(name, ['members']);
+		if (chat === undefined)
+			throw new NotFoundException();
+		return chat.members;
 	}
 
 	@Post()
