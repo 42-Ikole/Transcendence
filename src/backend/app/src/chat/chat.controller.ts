3dc526e4aaa720196dc41e0fa38637adc1d8c544
Mark Peerdeman <markpeerdeman@live.nl>
2022-04-12 08:47:15 +0200

    Feat: Improved the create chat

--- src/backend/app/src/chat/chat.controller.ts
+++ src/backend/app/src/chat/chat.controller.ts
@@ -20,6 +20,9 @@ export class ChatController {
 
 	@Get()
 	async findAll(): Promise<Chat[]> {
+		// Get all basic information about the chat rooms.
+
+		// TO DO: filter only rooms that should be seen, and split into two categories.
 		return await this.chatService.findAll();
 	}
 
@@ -31,6 +34,7 @@ export class ChatController {
 		type: String,
 	})
 	async findByName(@Param('name') name: string): Promise<Message[]> {
+		// Get all the messages for a particular chat.
 		const chat: Chat = await this.chatService.findByName(name, ['messages', 'messages.author']);
 		if (chat === undefined)
 			throw new NotFoundException();
@@ -45,6 +49,7 @@ export class ChatController {
 		type: String,
 	})
 	async findUsersForChat(@Param('name') name: string): Promise<User[]> {
+		// Get all the members for a particular chat.
 		const chat: Chat = await this.chatService.findByName(name, ['members']);
 		if (chat === undefined)
 			throw new NotFoundException();
@@ -53,13 +58,17 @@ export class ChatController {
 
 	@Post()
 	async createChat(@Req() request: RequestWithUser, @Body() body: CreateChatDto): Promise<Chat> {
+		// Create an interface from the DTO.
 		const createChatInterface: CreateChatInterface = {
 			...body,
 			owner: request.user,
+			members: [request.user],
 		};
+		// Add the chat to the database.
 		const chat: Chat = await this.chatService.createChat(createChatInterface);
-		this.socketService.chatServer.emit('createRoom', {ding: 'saus'});
-		console.log('emited naar createRoom');
+		// Broadcast the new room to everyone.
+		this.socketService.chatServer.emit('createRoom', {room: chat});
+		console.log('Emited createRoom');
 		return chat;
 	}
 }
