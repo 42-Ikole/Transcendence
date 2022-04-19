cc5c30b5c8f4ad3ddbda486379d371e5ec186a63
Mark Peerdeman <markpeerdeman@live.nl>
2022-04-19 11:18:06 +0200

    Feat: added Ban and User to Chat module

--- src/backend/app/src/chat/chat.module.ts
+++ src/backend/app/src/chat/chat.module.ts
@@ -7,9 +7,11 @@ import { Chat } from 'src/orm/entities/chat.entity';
 import { Message } from 'src/orm/entities/message.entity';
 import { CookieModule } from 'src/websocket/cookie.module';
 import { UserModule } from 'src/user/user.module';
+import { Mute } from 'src/orm/entities/mute.entity';
+import { Ban } from 'src/orm/entities/ban.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Chat, Message]), CookieModule, UserModule],
+  imports: [TypeOrmModule.forFeature([Chat, Message, Ban, Mute]), CookieModule, UserModule],
   exports: [ChatService],
   providers: [ChatService, ChatGateway],
   controllers: [ChatController],
